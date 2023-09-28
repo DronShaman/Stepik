eede4aaf5a43f69710868dac5c37fbb93182fb99
DronShaman <104376205+DronShaman@users.noreply.github.com>
2023-09-28 20:50:17 +0300

    28.09.2023

--- test.py
+++ test.py
@@ -1,29 +1,3 @@
-#5
-#Язык Python прекрасен
-#C# - отличный язык программирования
-#Stepik - отличная платформа
-#BEEGEEK FOREVER!
-#язык Python появился 20 февраля 1991
-#2
-#язык
-#python
-n = int(input())
-
-strings = []
-for _ in range(n):
-    s = input()
-    strings.append(s)
-
-k = int(input())
-
-search_queries = []
-for _ in range(k):
-    search_query = input()
-    search_queries.append(search_query)
-
-for s in strings:
-    for search_query in search_queries:
-        if search_query.lower() not in s.lower():
-            break
-    else:
-        print(s)
+a = 17 // (23 % 7)
+b = 34 % a * 5 - 29 % 4 * 3
+print(a * b)
