118f27c2740fe0d2168b7a54e4febdd97910f0f9
DronShaman <104376205+DronShaman@users.noreply.github.com>
2023-11-01 19:21:36 +0300

    Вечер 01 нояря

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
-import math
-
-n = int(input())
-
-a = 0
-for i in range(1, n+1):
-    a = a + (1 / i)
-
-print(a - math.log(n))
-
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[-9:])
