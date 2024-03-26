af53d7b9e333b78a5eb44f24986e2e808a8e1731
DronShaman <104376205+DronShaman@users.noreply.github.com>
2024-03-26 18:20:24 +0300

    22.03.2024

--- test-2024.py
+++ test-2024.py
@@ -1,17 +1,5 @@
-# объявление функции
-def get_next_prime(num):
-    while True:
-        num += 1
-        s = 0
-        for i in range(1, num + 1):
-            if num % i == 0:
-                s += 1
-        if s == 2:
-            return num
-        else:
-            s = 0
-# считываем данные
-n = int(input())
+import random
 
-# вызываем функцию
-print(get_next_prime(n))
+numbers = [1, 2, 4, 6, 7, 9]
+
+rand_numbers = random.sample(numbers, 3)
